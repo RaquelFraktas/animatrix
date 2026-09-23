56cc9f21e96564b45efb58eb35c58ccb179cbd1e
Raquel <72051783+RaquelFraktas@users.noreply.github.com>
2026-09-23 16:30:25 -0400

    test

--- test/controllers/scan_controller_test.rb
+++ test/controllers/scan_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ScanControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "saving an unnamed player redirects to the users index" do
+    user = User.create!(qr_code: "scan-name-test")
+
+    post scan_path, params: {
+      qr_code: user.qr_code,
+      target_name: "Grace"
+    }
+
+    assert_redirected_to users_path
+    assert_equal "Grace", user.reload.name
+  end
 end
